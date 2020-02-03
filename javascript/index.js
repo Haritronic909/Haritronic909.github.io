ff180f6043b1b2c1e2bc427c8bdb9ff75b4002d8
K Harishankar <48408097+Haritronic909@users.noreply.github.com>
2020-02-03 23:10:55 +0530

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,16 @@
+var n = 0;
+
+function increment(){
+
+  n++;
+  return n;
+      
+}
+
 function counter_fn() {
       var counter = document.getElementById("cntr");
-      var count = 0;
+      count =n;
       count = parseInt(counter.innerHTML);
-      count = count + 1;
       counter.innerHTML = count;
     }
     window.onload = counter_fn;
