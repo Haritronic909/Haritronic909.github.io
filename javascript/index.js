6cacd5847d2c070f045236cca868a272261cf6e4
K Harishankar <48408097+Haritronic909@users.noreply.github.com>
2020-02-03 22:59:34 +0530

    Update index.js

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,8 @@
-function counter_fn(){
-       var counter =  $('#cntr span').text();
-       var count = 0;
-       count = parseInt(counter.value);
-       count = count+1;
-       counter.innerHTML = parseInt(count);
+function counter_fn() {
+      var counter = document.getElementById("cntr");
+      var count = 0;
+      count = parseInt(counter.innerHTML);
+      count = count + 1;
+      counter.innerHTML = count;
     }
-    window.onload = counter_fn;  
+    window.onload = counter_fn;
